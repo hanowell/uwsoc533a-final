90801098aaac6974f3b4372546aa541c47f12adf
Ben Hanowell <ben@hanowell.me>
2022-03-09 16:38:23 -0800

    Update data sets

--- R/data-collection.R
+++ R/data-collection.R
@@ -3,8 +3,11 @@
 # Setup ----
 library(dplyr)
 library(HMDHFDplus)
+library(readr)
+library(tidyr)
 
-# US age-specific start-of-year population counts in 2006 and 2019 ----
+# US age-specific start-of-year population counts ----
+periods <- c(1918, 1943, 1968, 2005, 2015, 2016, 2019)
 us_population <- HMDHFDplus::readHMDweb(
   CNTRY = "USA",
   item = "Population5",
@@ -14,9 +17,9 @@ us_population <- HMDHFDplus::readHMDweb(
     username = keyring::key_list("human-mortality-database")$username
   )
 ) %>%
-  dplyr::filter(Year %in% c(2006, 2019))
+  dplyr::filter(Year %in% periods)
 
-# US age-specific exposures in 2006 and 2019 ----
+# US age-specific exposures ----
 us_exposures <- HMDHFDplus::readHMDweb(
   CNTRY = "USA",
   item = "Exposures_5x1",
@@ -26,9 +29,9 @@ us_exposures <- HMDHFDplus::readHMDweb(
     username = keyring::key_list("human-mortality-database")$username
   )
 ) %>%
-  dplyr::filter(Year %in% c(2006, 2019))
+  dplyr::filter(Year %in% periods)
 
-# US age-specific mortality in 2006 and 2019 ----
+# US age-specific mortality ----
 us_asmr <- HMDHFDplus::readHMDweb(
   CNTRY = "USA",
   item = "Mx_5x1",
@@ -38,9 +41,59 @@ us_asmr <- HMDHFDplus::readHMDweb(
     username = keyring::key_list("human-mortality-database")$username
   )
 ) %>%
-  dplyr::filter(Year %in% c(2006, 2019))
+  dplyr::filter(Year %in% periods)
 
-# US age-specific fertility in 2006 and 2019 ----
+# US age-specific deaths ----
+us_deaths <- HMDHFDplus::readHMDweb(
+  CNTRY = "USA",
+  item = "Deaths_5x1",
+  username = keyring::key_list("human-mortality-database")$username,
+  password = keyring::key_get(
+    service = "human-mortality-database",
+    username = keyring::key_list("human-mortality-database")$username
+  )
+) %>%
+  dplyr::filter(Year %in% periods)
+
+# US cause- and age-specific mortality ----
+us_deaths_despair <- readr::read_csv("raw/USA_d_interm_idr.csv") %>%
+  dplyr::mutate(despair = cause %in% c(40, 41, 76, 99)) %>%
+  dplyr::filter(despair, year == 2015, sex == 3) %>%
+  dplyr::select(starts_with("d"), -d85p, -d90p, -d95p, -despair) %>%
+  dplyr::summarize_all(sum) %>%
+  tidyr::pivot_longer(
+    cols = starts_with("d"),
+    names_to = "Age",
+    names_prefix = "d",
+    values_to = "D_despair"
+  ) %>%
+  dplyr::mutate(Age = as.integer(stringr::str_replace_all(Age, "p", "")))
+
+# U.S. death counts by Lexis triangles ----
+us_deaths_lexis <- HMDHFDplus::readHMDweb(
+  CNTRY = "USA",
+  item = "Deaths_lexis",
+  username = keyring::key_list("human-mortality-database")$username,
+  password = keyring::key_get(
+    service = "human-mortality-database",
+    username = keyring::key_list("human-mortality-database")$username
+  )
+) %>%
+  dplyr::filter(Year %in% c(2015:2016))
+
+# U.S. person-years by Lexis triangles ----
+us_exposures_lexis <- HMDHFDplus::readHMDweb(
+  CNTRY = "USA",
+  item = "Exposures_lexis",
+  username = keyring::key_list("human-mortality-database")$username,
+  password = keyring::key_get(
+    service = "human-mortality-database",
+    username = keyring::key_list("human-mortality-database")$username
+  )
+) %>%
+  dplyr::filter(Year %in% c(2015:2016))
+
+# US age-specific fertility ----
 us_asfr <- HMDHFDplus::readHFDweb(
   CNTRY = "USA",
   item = "asfrRR",
@@ -50,10 +103,10 @@ us_asfr <- HMDHFDplus::readHFDweb(
     username = keyring::key_list("human-fertility-database")$username
   )
 ) %>%
-  dplyr::filter(Year %in% c(2006, 2019))
+  dplyr::filter(Year %in% periods)
 
-# US age-specific births in 2006 and 2019 ----
-us_asfr <- HMDHFDplus::readHFDweb(
+# US age-specific births ----
+us_births <- HMDHFDplus::readHFDweb(
   CNTRY = "USA",
   item = "birthsRR",
   username = keyring::key_list("human-fertility-database")$username,
@@ -62,10 +115,10 @@ us_asfr <- HMDHFDplus::readHFDweb(
     username = keyring::key_list("human-fertility-database")$username
   )
 ) %>%
-  dplyr::filter(Year %in% c(2006, 2019))
+  dplyr::filter(Year %in% periods)
 
 # US parity progression ratios for birth cohorts 1918, 1943, and 1968 ----
-us_ppr <-  HMDHFDplus::readHFDweb(
+us_ppr <- HMDHFDplus::readHFDweb(
   CNTRY = "USA",
   item = "pprVHbo",
   username = keyring::key_list("human-fertility-database")$username,
